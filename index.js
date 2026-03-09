b1a476523270bde70bc9338ce7a88687e7f0d865
natebeams <natecat41k@gmail.com>
2026-03-08 21:21:45 -0700

    fixed double express server

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
 require("dotenv").config();
 
-/* start bot */
-const client = require("./bot");
+const { Client, GatewayIntentBits } = require("discord.js");
 
-/* start dashboard */
+const client = new Client({
+intents: [
+GatewayIntentBits.Guilds,
+GatewayIntentBits.GuildMessages
+]
+});
+
 require("./dashboard")(client);
+
+client.once("ready", () => {
+console.log(`Logged in as ${client.user.tag}`);
+});
+
+client.login(process.env.TOKEN);
