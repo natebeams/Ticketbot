2b888ee40a37b47cdc0be34c9ff90d07e0e6cd91
natebeams <natecat41k@gmail.com>
2026-03-08 21:03:10 -0700

    fixed double express server

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ app.get("/test",(req,res)=>{
 res.send("dashboard working");
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 
 app.listen(PORT, () => {
 console.log(`Web server running on port ${PORT}`);
